6f01c72d86179fddb9a77fbcbbbcb2328c353e42
febri <66045089+melisafebriani@users.noreply.github.com>
2021-01-13 14:21:54 +0700

    Update Multi_LineString.php

--- Multi_LineString.php
+++ Multi_LineString.php
@@ -6,6 +6,6 @@ SELAMAT BELAJAR PHP
 
 Sekarang kita akan belajar tipe data string
 Ini adalah salah satu cara membuat String 
-bisa menggunakan "HEREDOC"...!
+bisa menggunakan "heredoc"...!
 
 MELISA;
